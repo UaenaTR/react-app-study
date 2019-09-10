13eb245021574e838f0321cf9306488048be3b56
SmileTR <tr580230@163.com>
2019-09-10 16:06:17 +0800

    react study list

--- src/components/hello.js
+++ src/components/hello.js
@@ -6,24 +6,66 @@ class Hello extends Component {
   this.state = {
     msg: "Hello Msg",
     Component: 'Hello',
-    count: 0
+    count: 0,
+    type: 'pink',
+    iptValue: ''
   }
   this.change = this.change.bind(this)
+  this.handleChange = this.handleChange.bind(this)
+  this.handleSubmit = this.handleSubmit.bind(this)
+  this.handleIptChange = this.handleIptChange.bind(this)
  }
 //  componentDidMount() {
 //     console.log(this)
 //  }
- change(){
+ componentDidUpdate(){
+   console.log(this.state.iptValue)
+ } 
+ change(e){
+   e.persist()
    this.setState((state,props) => ({
     count: state.count+1,
     Component: props.user + state.count
    }))
+   console.log(e)
+ }
+ handleChange(e){
+  e.persist()
+  this.setState((state, props) => {
+    return {
+      type: e.target.value
+    }
+  })
+ }
+ handleIptChange(e){
+   e.persist()
+      this.setState((state, props) => {
+      return {
+        iptValue: e.target.value
+      }
+    })
+ }
+ handleSubmit(e){
+   console.log(this.state)
+   e.preventDefault()
  }
  render(){
+  const arr = [1,2,3,4,5]
+  const list = arr.map(item => <div key={item}>{item}</div>)
   return (
     <div className="hello">
+      {list}
       <div className="state">{this.state.msg}--------{this.state.Component}--------{this.state.count}</div>
       <span className="props" onClick={this.change}>{this.props.user}</span>
+      <form onSubmit={this.handleSubmit}>
+        <input type="text" value={this.state.iptValue} onChange={this.handleIptChange}/>
+        <select value={this.state.type} onChange={this.handleChange}>
+          <option value="white">白色</option>
+          <option value="yellow">黄色</option>
+          <option value="pink">粉色</option>
+        </select>
+        <input type="submit"/>
+      </form>
     </div>   
   )
  }
